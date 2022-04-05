1d64f9003a417c26ed4f7318541de00a6157e418
TJ <62122206+tjvsx@users.noreply.github.com>
2022-04-04 21:17:24 -0400

    Update TokenMinter.sol

--- contracts/upgrades/proposals/TokenMinter.sol
+++ contracts/upgrades/proposals/TokenMinter.sol
@@ -20,6 +20,9 @@ with this approach, we can have a set of external proposal contracts,
 each with the execute() function with a specific functionality,
 whether that functionality is minting, burning, transfering, cutting in/out facets, etc  
 
-the diamond is callable in a specific way (no function collisions allowed), but the diamond can call other contracts in an arbitrary way, so function selectors can be preset in a call within the diamond's facet (ex: governance.execute()) and calling it can lead to many different functionalities. 
+the diamond is callable in a specific way (no function collisions allowed), 
+but the diamond can call other contracts in an arbitrary way, 
+so function selectors can be preset in a call within the diamond's facet (ex: governance.execute()) 
+and calling it can lead to many different functionalities. 
 -- with this, must consider possible security issues and whether it's gas-optimal
 */
